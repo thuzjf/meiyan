6a4be12dd3d40ffb99a3de1ce3d47a3a2b423e11
jianfeng.zhang <chinazjf@vip.qq.com>
2018-08-31 00:40:34 +0800

    test sync on windows

--- utils.py
+++ utils.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2018/8/30 上午2:19
+# @Time    : 2018/8/15 22:00
 # @Author  : jf.zhang
-# @Email   : chinazjf@vip.qq.com
-# @File    : utils.py
+# @File    : classification.py
+# @Software: PyCharm
 
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-from PIL import Image
 
+t=np.linspace(0,10, 365)
+x=t**2
+r=np.random.randn(365)*10
+y=x+r
+plt.plot(t,y)
+z=[y[0]]
+z2=[y[0]]
+for i in range(len(y)-1):
+    z.append(z[i]*0.9+y[i+1]*0.1)
+    z2.append(z2[i]*0.98+y[i+1]*0.02)
+plt.plot(t,z)
+plt.plot(t,z2)
+plt.legend(['y1','y2','y3'])
+plt.show()
 
