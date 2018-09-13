54fe23146847eb252e1a72c8c6fb81a9e5ee4b68
jianfeng.zhang <chinazjf@vip.qq.com>
2018-09-14 01:19:53 +0800

    optimise the test pipeline

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ import numpy as np
 from PIL import Image
 
 
-def detect_mopi(ori_array, mopi_array):
+def detect_mopi(ori_array, im_array):
   """
   detect whether mopi has started, based on the fact that mopi will smooth the picture
    and it's gradients at sharp points will get turned down.
@@ -22,7 +22,7 @@ def detect_mopi(ori_array, mopi_array):
   """
   grad_ori_x, grad_ori_y = np.gradient(ori_array)
   grad_ori = np.sqrt(grad_ori_x**2 + grad_ori_y**2).flatten()
-  grad_mopi_x, grad_mopi_y = np.gradient(mopi_array)
+  grad_mopi_x, grad_mopi_y = np.gradient(im_array)
   grad_mopi = np.sqrt(grad_mopi_x**2 + grad_mopi_y**2).flatten()
 
   grad_ori_low = np.zeros_like(grad_ori)
@@ -37,28 +37,49 @@ def detect_mopi(ori_array, mopi_array):
     return False, increment
 
 
+def detect_dayan(ori_array, im_array):
+  pass
+  
+  
+  
 
 if __name__ == "__main__":
   im_dir = "./resource"
-  ori_path = os.path.join(im_dir, '0.png')
-  shoulian_path = os.path.join(im_dir, 'shoulian_max.png')
-  hongrun_path = os.path.join(im_dir, "hongrun_max.png")
-  mopi_path = os.path.join(im_dir, "mopi_max.png")
-
+  filelist =[f for f in os.listdir(im_dir) if 'png' in f]
+  ori_img_name = "ori.png"
+  ori_path = os.path.join(im_dir, ori_img_name)
   ori_im = Image.open(ori_path).convert('L')
-  shoulian_im = Image.open(shoulian_path).convert('L')
-  mopi_im = Image.open(mopi_path).convert('L')
-
   ori_array = np.array(ori_im)
-  shoulian_array = np.array(shoulian_im)
-  mopi_array = np.array(mopi_im)
 
-  state_mp, increment = detect_mopi(ori_array, mopi_array)
-  if state_mp:
-    print "mopi has successfully started! detected low gradient increment is {}",format(increment)
+  #begin test
+  filelist.remove(ori_img_name)
+  for item in filelist:
+    im_path = os.path.join(im_dir, item)
+    im = Image.open(im_path).convert('L')
+    im_array = np.array(im)
+    # test mopi
+    if "mopi" in item:
+      state_mp, increment = detect_mopi(ori_array, im_array)
+      if state_mp:
+        print "successfully detected mopi! detected low gradient increment is {:.3f}".format(increment)
+      else:
+        print "failed to detect mopi! detected low gradient increment is {:.3f}".format(increment)
 
+    if "dayan" in item:
+      detect_dayan(ori_img_name, im_array)
+      pass
+    
+    if "hongrun" in item:
+      pass
+    
+    if "meibai" in item:
+      pass
+    
+    if "saihong" in item:
+      pass
+    
+    if "shoulian" in item:
+      pass
 
-
-
-
+  print "all tests finished!"
 
